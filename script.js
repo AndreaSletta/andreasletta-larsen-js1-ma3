f5370a0906d4a1ffc4b1f95c64797eb7af058b4f
AndreaSletta <70203798+AndreaSletta@users.noreply.github.com>
2021-02-05 11:40:21 +0100

    Final

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 //Question 1
 
-const division = (a, b) =>  a % b;
-
+const division = (a, b) => a % b;
 
 //Question 2
 /* Make a call to the following API endpoint:
@@ -11,6 +10,7 @@ Loop through the results and display the following properties in HTML, but only 
 name
 rating
 number of tags (not the tag details, just the amount of tags)
+
 The styling for this assignment is not important but loading indicator should be displayed while the API call is in progress.
 
 Be sure to handle any potential errors in the code.
@@ -19,14 +19,48 @@ You can use either regular promise or async/await syntax to make the call.
 
 Be sure to arrange all file types appropriately, consult the repos from the lessons for examples.*/
 
+const url = 'https://api.rawg.io/api/games?dates=2019-01-01,2019-12-31&ordering=-rating';
 
+const videogameContainer = document.querySelector('.container');
 
-const url = "https://api.rawg.io/api/games?dates=2019-01-01,2019-12-31&ordering=-rating";
-const corsEnabledUrl = "https://noroffcors.herokuapp.com/" + url;
+async function makeApiCall() {
+	try {
+		const response = await fetch(url);
 
-async function makeApiCall(){
+		const callResult = await response.json();
 
-const response = await fetch(corsEnabledUrl);
-const results = await response.json();
-console.log(results);
+		const videogame = callResult.results;
+
+		videogameContainer.innerHTML = '';
+
+		for (let i = 0; i < 8; i++) {
+			const name = videogame[i].name;
+
+			const rating = videogame[i].rating;
+
+			const tagCount = videogame[i].tags.length;
+
+			videogameContainer.innerHTML +=
+				'<div>' +
+				'<ul>' +
+				'<li>' +
+				'Name: ' +
+				name +
+				'</li> ' +
+				'<li>' +
+				'Rating: ' +
+				rating +
+				'</li> ' +
+				'<li>' +
+				'Tags: ' +
+				tagCount +
+				'</li> ' +
+				'</ul>' +
+				'</div>';
+		}
+	} catch (error) {
+		videogameContainer.innerHTML = 'Error loading page!';
+	}
 }
+
+makeApiCall();
