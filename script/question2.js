fe4c84b7cc7ecf9675474f0b5b2506ac33a7083e
AndreaSletta <70203798+AndreaSletta@users.noreply.github.com>
2021-02-05 11:49:50 +0100

    This really is the final

---
+++ script/question2.js
@@ -0,0 +1,47 @@
+//Question 2
+
+const url = 'https://api.rawg.io/api/games?dates=2019-01-01,2019-12-31&ordering=-rating';
+
+const videogameContainer = document.querySelector('.container');
+
+async function makeApiCall() {
+	try {
+		const response = await fetch(url);
+
+		const callResult = await response.json();
+
+		const videogame = callResult.results;
+
+		videogameContainer.innerHTML = '';
+
+		for (let i = 0; i < 8; i++) {
+			const name = videogame[i].name;
+
+			const rating = videogame[i].rating;
+
+			const tagCount = videogame[i].tags.length;
+
+			videogameContainer.innerHTML +=
+				'<div>' +
+				'<ul>' +
+				'<li>' +
+				'Name: ' +
+				name +
+				'</li> ' +
+				'<li>' +
+				'Rating: ' +
+				rating +
+				'</li> ' +
+				'<li>' +
+				'Tags: ' +
+				tagCount +
+				'</li> ' +
+				'</ul>' +
+				'</div>';
+		}
+	} catch (error) {
+		videogameContainer.innerHTML = 'Error loading page!';
+	}
+}
+
+makeApiCall();
